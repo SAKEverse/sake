4dc3aa8887fb460ba762cc8af00bc38824a107ef
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2023-04-18 10:14:55 -0400

    Also look for conda in AppData\Local

--- sakeconnectivity/sake_connectivity.cmd
+++ sakeconnectivity/sake_connectivity.cmd
@@ -1,11 +1,29 @@
 :: Check if Miniconda is present
 IF EXIST %USERPROFILE%\miniconda3\Scripts\activate.bat (
     set conda_file=%USERPROFILE%\miniconda3
-) ELSE (
-	IF EXIST %USERPROFILE%\anaconda3\Scripts\activate.bat (
-    	set conda_file=%USERPROFILE%\anaconda3
+    GOTO FOUND
 )
+
+IF EXIST %USERPROFILE%\AppData\Local\miniconda3\Scripts\activate.bat (
+    set conda_file=%USERPROFILE%\AppData\Local\miniconda3
+    GOTO FOUND
 )
+
+IF EXIST %USERPROFILE%\AppData\Local\anaconda3\Scripts\activate.bat (
+    set conda_file=%USERPROFILE%\AppData\Local\anaconda3
+    GOTO FOUND
+)
+
+IF EXIST %USERPROFILE%\anaconda3\Scripts\activate.bat (
+    set conda_file=%USERPROFILE%\anaconda3
+    GOTO FOUND
+)
+
+echo Miniconda or Anaconda not found.
+pause
+exit /b
+
+:FOUND
 
 :: enter conda
 call %conda_file%\Scripts\activate.bat
